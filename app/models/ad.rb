9ef75f1e78b435c9aa5625a75a2afc81272fca32
Aravind <pro.aravind@gmail.com>
2014-09-11 16:38:17 +0530

    some basic association changes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,7 +1,7 @@
 class Ad < ActiveRecord::Base
   belongs_to :user,  inverse_of: :ads
   belongs_to :category, inverse_of: :ads
-  has_many :ad_image_attachments
+  has_many :ad_image_attachments,  :inverse_of => :ad
   accepts_nested_attributes_for :ad_image_attachments,  :reject_if => :all_blank,  :allow_destroy => true
 
   validates :name, presence: true
--- app/models/ad_image_attachment.rb
+++ app/models/ad_image_attachment.rb
@@ -1,4 +1,4 @@
 class AdImageAttachment < ActiveRecord::Base
-  belongs_to :ad
+  belongs_to :ad,  :inverse_of => :ad_image_attachments
   mount_uploader :image,  AdImageUploader
 end
